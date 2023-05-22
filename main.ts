f002bcda0ccf7160f743409ddd85f4ff59d0abc3
donfii <130453085+donfii@users.noreply.github.com>
2023-05-22 09:30:43 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(steps)
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    steps = 0
+    basic.showNumber(steps)
+})
+let steps = 0
+steps = 0
+basic.forever(function () {
+    if (input.acceleration(Dimension.Strength) > 1500) {
+        steps += 1
+    }
+})
