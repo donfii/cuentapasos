bc728f2dd840ebab48f116fddf5d2110b2fb6c04
donfii <130453085+donfii@users.noreply.github.com>
2023-05-29 09:26:16 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,15 +4,50 @@ input.onButtonPressed(Button.A, function () {
 input.onGesture(Gesture.Shake, function () {
 	
 })
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(metros)
+})
 input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
     steps = 0
 })
+let metros = 0
 let steps = 0
 basic.clearScreen()
 steps = 0
 basic.forever(function () {
-    if (input.acceleration(Dimension.Strength) > 1500) {
+    if (input.acceleration(Dimension.Z) > 1500) {
+        steps += 0.5
+        basic.showNumber(steps)
+    }
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) > 1500) {
+        steps += 2
+        basic.showNumber(steps)
+    }
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.Y) > 1500) {
         steps += 1
+        basic.showNumber(steps)
+    }
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.Y) > 1500) {
+        metros += 2.5
+        basic.showNumber(steps)
+    }
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.Z) > 1500) {
+        metros += 1.75
+        basic.showNumber(steps)
+    }
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) > 1500) {
+        metros += 2
+        basic.showNumber(steps)
     }
 })
